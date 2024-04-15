b4f2dbd653dee5721173f702311ce5dcf005eb48
chenshane2013@gmail.com <sean>
2024-04-15 10:09:31 +0800

    ENABLE_RATE_LIMIT default false

--- electrumx/server/env.py
+++ electrumx/server/env.py
@@ -96,7 +96,7 @@ class Env(EnvBase):
         self.session_group_by_subnet_ipv4 = self.integer('SESSION_GROUP_BY_SUBNET_IPV4', 24)
         self.session_group_by_subnet_ipv6 = self.integer('SESSION_GROUP_BY_SUBNET_IPV6', 48)
         self._check_and_fix_cost_limits()
-        self.enable_rate_limit = self.boolean('ENABLE_RATE_LIMIT', True)
+        self.enable_rate_limit = self.boolean('ENABLE_RATE_LIMIT', False)
         
         # Services last - uses some env vars above
 
